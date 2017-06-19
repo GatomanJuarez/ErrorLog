568cd40c5d64761831547ce2af95ba8be5b9fb28
GatomanJuarez <juarez_devora_ja@hotmail.com>
2017-06-19 09:14:29 -0500

    Vamo a programar

---
+++ ErrorLog/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ErrorLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(80, 43);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(94, 120);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ ErrorLog/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+
+namespace ErrorLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection Cnn = new MySqlConnection();
+        List<String> lista = new List<string>();
+
+        public void LogFile(string error)
+
+        {
+
+            StreamWriter log;
+
+            if (!File.Exists("logfile.txt"))
+
+            {
+
+                log = new StreamWriter("logfile.txt");
+
+            }
+
+            else
+
+            {
+
+                log = File.AppendText("logfile.txt");
+
+            }
+
+            // Write to the file:
+
+            log.WriteLine("Data Time:" + DateTime.Now);
+
+            log.WriteLine("Exception Name:" + error);
+
+
+
+
+
+            log.Close();
+
+        }
+
+        private void guardarPalabra()
+        {
+            try
+            {
+                Cnn.ConnectionString = "Server = localhost; Database = conjuntosYo; User Id = root; Password =; ";
+            }
+            catch (Exception e)
+            {
+               LogFile(e.ToString ());
+            }
+        }
+
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            guardarPalabra();
+        }
+    }
+
+    public static class ExceptionHelper
+
+    {
+
+        public static int LineNumber(this Exception e)
+
+        {
+
+            int linenum = 0;
+
+            try
+
+            {
+
+                linenum = Convert.ToInt32(e.StackTrace.Substring(e.StackTrace.LastIndexOf(":line") + 5));
+
+            }
+
+            catch
+
+            {
+
+                //Stack trace is not available!
+
+            }
+
+            return linenum;
+
+        }
+
+    }
+}
